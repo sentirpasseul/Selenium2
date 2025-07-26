f113b4c87f3b0df8ca2a040f999658e19020b1b6
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-07-26 16:22:31 +0400

    Реализация BasePage и Singleton Driver

---
+++ pages/base_page.py
@@ -0,0 +1,13 @@
+from core.driver import WebDriver
+
+
+class BasePage:
+    TIMEOUT = 5
+
+    def __init__(self, driver, url):
+        self.driver = driver
+
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
